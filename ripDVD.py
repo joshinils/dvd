e83cf15c2ec665eae0dfc52a12311f3369514722
joshinils <joshua.laskowski@web.de>
2020-10-23 11:42:16 +0200

    rename vars

--- ripDVD.py
+++ ripDVD.py
@@ -11,104 +11,101 @@ import sys
 parser = argparse.ArgumentParser(
     description='copies a dvd to disc, no processing. The output will be named by the current date and time.')
 
-requiredNamed = parser.add_argument_group('required arguments')
-requiredNamed.add_argument('-d', '--driveNo', help='drive number, ie \'/dev/sr0\' means \'-d 0\'', required=True)
+required_named = parser.add_argument_group('required arguments')
+required_named.add_argument('-d', '--driveNo', help='drive number, ie \'/dev/sr0\' means \'-d 0\'', required=True)
 parser.add_argument('-f', type=str, default="DVD", help='filename to prepend the standard name, default="DVD"')
 
 args = parser.parse_args()
 
-driveNumber = args.driveNo
-assert driveNumber != None, "why u put no drive number, that dum"
+drive_number = args.driveNo
+assert drive_number != None, "why u put no drive number, that dum"
 
-fileName = args.f
+file_name = args.f
 
 
-def driveExists(driveNumber: int) -> bool:
-    subProcessResult = subprocess.run(['setcd', '-i', '/dev/sr'+str(driveNumber)],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-    subProcessStdOut = subProcessResult.stdout.decode('utf-8')
-    subProcessStdErr = subProcessResult.stderr.decode('utf-8')
-    #print("subProcessStdOut", subProcessStdOut)
-    #print("subProcessStdErr", subProcessStdErr)
-    return 'No such file or directory' not in subProcessStdErr
+def drive_exists(drive_number: int) -> bool:
+    sub_process_result = subprocess.run(['setcd', '-i', '/dev/sr'+str(drive_number)],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+    sub_process_std_out = sub_process_result.stdout.decode('utf-8')
+    sub_process_std_err = sub_process_result.stderr.decode('utf-8')
+    #print("sub_process_std_out", sub_process_std_out)
+    #print("sub_process_std_err", sub_process_std_err)
+    return 'No such file or directory' not in sub_process_std_err
 
 
 # only proceed if the drive exists
-assert driveExists(driveNumber), "drive /dev/sr" + str(driveNumber) + " doesn't exist!"
+assert drive_exists(drive_number), "drive /dev/sr" + str(drive_number) + " doesn't exist!"
 
 
-def driveFull(driveNumber: int) -> bool:
-    subProcessResult = subprocess.run(['setcd', '-i', '/dev/sr'+str(driveNumber)],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-    subProcessStdOut = subProcessResult.stdout.decode('utf-8')
-    subProcessStdErr = subProcessResult.stderr.decode('utf-8')
-    return 'Disc found in drive' in subProcessStdOut
+def drive_full(drive_number: int) -> bool:
+    sub_process_result = subprocess.run(['setcd', '-i', '/dev/sr'+str(drive_number)],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+    sub_process_std_out = sub_process_result.stdout.decode('utf-8')
+    return 'Disc found in drive' in sub_process_std_out
 
 
-def driveReady(driveNumber: int) -> bool:
-    subProcessResult = subprocess.run(['setcd', '-i', '/dev/sr'+str(driveNumber)],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-    subProcessStdOut = subProcessResult.stdout.decode('utf-8')
-    subProcessStdErr = subProcessResult.stderr.decode('utf-8')
-    return 'Drive is not ready' not in subProcessStdOut
+def drive_ready(drive_number: int) -> bool:
+    sub_process_result = subprocess.run(['setcd', '-i', '/dev/sr'+str(drive_number)],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+    sub_process_std_out = sub_process_result.stdout.decode('utf-8')
+    return 'Drive is not ready' not in sub_process_std_out
 
 
-def driveOpen(driveNumber: int) -> bool:
-    subProcessResult = subprocess.run(['setcd', '-i', '/dev/sr'+str(driveNumber)],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-    subProcessStdOut = subProcessResult.stdout.decode('utf-8')
-    subProcessStdErr = subProcessResult.stderr.decode('utf-8')
-    return 'tray is open' in subProcessStdOut
+def drive_open(drive_number: int) -> bool:
+    sub_process_result = subprocess.run(['setcd', '-i', '/dev/sr'+str(drive_number)],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+    sub_process_std_out = sub_process_result.stdout.decode('utf-8')
+    return 'tray is open' in sub_process_std_out
 
 
-#print("driveOpen", driveOpen(driveNumber))
-#print("driveReady", driveReady(driveNumber))
-#print("driveFull", driveFull(driveNumber))
+#print("drive_open", drive_open(drive_number))
+#print("drive_ready", drive_ready(drive_number))
+#print("drive_full", drive_full(drive_number))
 
 
-def waitOnClosedDrive():
+def wait_on_closed_drive():
     waiting = 1
-    while driveOpen(driveNumber):
+    while drive_open(drive_number):
         waiting += 1
-        print("\rdrive /dev/sr" + str(driveNumber) + " is open, waiting for you to close it with a disc inserted. ", end="")
+        print("\rdrive /dev/sr" + str(drive_number) + " is open, waiting for you to close it with a disc inserted. ", end="")
         spinner = "←↖↑↗→↘↓↙"
         print(spinner[waiting % len(spinner)], end="\r")
     sys.stdout.write("\033[K")  # Clear to the end of line
 
 
-def waitOnReadyDrive():
+def wait_on_ready_drive():
     waiting = 1
-    while not driveReady(driveNumber):
+    while not drive_ready(drive_number):
         waiting += 1
-        print("\rdrive /dev/sr" + str(driveNumber) + " is not ready, waiting for it to be ready. ", end="")
+        print("\rdrive /dev/sr" + str(drive_number) + " is not ready, waiting for it to be ready. ", end="")
         spinner = "←↖↑↗→↘↓↙"
         print(spinner[waiting % len(spinner)], end="\r")
     sys.stdout.write("\033[K")  # Clear to the end of line
 
 
-waitOnReadyDrive()
-while not driveFull(driveNumber):
-    if not driveOpen(driveNumber):
-        print("drive /dev/sr" + str(driveNumber) + " does not contain a disc, opening it for you now. ")
-        subprocess.run(['eject', '/dev/sr'+str(driveNumber)])
-    waitOnClosedDrive()
+wait_on_ready_drive()
+while not drive_full(drive_number):
+    if not drive_open(drive_number):
+        print("drive /dev/sr" + str(drive_number) + " does not contain a disc, opening it for you now. ")
+        subprocess.run(['eject', '/dev/sr'+str(drive_number)])
+    wait_on_closed_drive()
     time.sleep(1)
-    waitOnReadyDrive()
+    wait_on_ready_drive()
     print("", end="")
 
 # at this point the drive should be ready and there should be a disc in it
 
-subProcessResult = subprocess.run(['date', '+"%Y-%m-%dT%H%M%S"'],
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE)
-now_string = subProcessResult.stdout.decode('utf-8')[1:-2]
+sub_process_result = subprocess.run(['date', '+"%Y-%m-%dT%H%M%S"'],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+now_string = sub_process_result.stdout.decode('utf-8')[1:-2]
 print(now_string)
 
-subProcessResult = subprocess.run(['mplayer', '/dev/sr' + str(driveNumber), '-v',
-                                   '-dumpstream', '-dumpfile', fileName + '_' + now_string + '.avi'])
+subprocess.run(['mplayer', '/dev/sr' + str(drive_number), '-v',
+                '-dumpstream', '-dumpfile', file_name + '_' + now_string + '.avi'])
 
-subprocess.run(['eject', '/dev/sr'+str(driveNumber)])
+subprocess.run(['eject', '/dev/sr'+str(drive_number)])
